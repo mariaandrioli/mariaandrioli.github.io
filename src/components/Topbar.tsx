af665944a232d28bba9e91adfcb8a780ea957dfb
maria teresa <mariakandrioli@gmail.com>
2024-01-19 19:16:23 -0300

    feat: hamburguer menu

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,12 +1,25 @@
+import ContactPageOutlinedIcon from "@mui/icons-material/ContactPageOutlined";
+import Face3OutlinedIcon from "@mui/icons-material/Face3Outlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
 import {
   Box,
   Button,
+  Drawer,
+  Grid,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
   ThemeProvider,
   Typography,
   createTheme,
+  useMediaQuery,
 } from "@mui/material";
-import Grid from "@mui/system/Unstable_Grid";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const headingFont = createTheme({
@@ -22,12 +35,16 @@ interface Props {
 export const Topbar = ({ LanguageToggle }: Props) => {
   const { t } = useTranslation();
 
+  const [openDrawer, toggleDrawer] = useState(false);
+
+  const isMobile = useMediaQuery("(max-width:600px)");
+
   const buttonCustomStyle = {
     borderRadius: "3rem",
     px: "1.5rem",
     backgroundColor: "#fff",
     color: "#563d80",
-    letterSpacing: '0.005rem',
+    letterSpacing: "0.005rem",
     "&:hover": {
       backgroundColor: "#bd98e0", // Change the hover background color
       color: "#fff",
@@ -42,23 +59,81 @@ export const Topbar = ({ LanguageToggle }: Props) => {
         justifyContent="space-between"
         alignItems="flex-start"
       >
-        <Grid xs={2}>
-          <ThemeProvider theme={headingFont}>
-            <Typography
-              variant="h6"
-              sx={{ display: { xs: "none", sm: "block" } }}
-            >
-              Maria Teresa Andrioli
-            </Typography>
-          </ThemeProvider>
-        </Grid>
-        <Grid xs={8}>
-          <Box display={"flex"} gap={1} justifyContent={"center"}>
-            <Button sx={buttonCustomStyle}>{t("menuHome")}</Button>
-            <Button sx={buttonCustomStyle}>{t("menuAbout")}</Button>
-            <Button sx={buttonCustomStyle}>{t("menuContact")}</Button>
+        {!isMobile && (
+          <Grid xs={2}>
+            <ThemeProvider theme={headingFont}>
+              <Typography
+                variant="h6"
+                sx={{ display: { xs: "none", sm: "block" } }}
+              >
+                Maria Teresa Andrioli
+              </Typography>
+            </ThemeProvider>
+          </Grid>
+        )}
+
+        <Hidden smDown>
+          <Grid xs={8}>
+            <Box display={"flex"} gap={1} justifyContent={"center"}>
+              <Button sx={buttonCustomStyle}>{t("menuHome")}</Button>
+              <Button sx={buttonCustomStyle}>{t("menuAbout")}</Button>
+              <Button sx={buttonCustomStyle}>{t("menuContact")}</Button>
+            </Box>
+          </Grid>
+        </Hidden>
+
+        <Hidden smUp>
+          <IconButton onClick={() => toggleDrawer(!openDrawer)}>
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
+
+        <Drawer
+          anchor={"bottom"}
+          open={openDrawer}
+          onClose={() => toggleDrawer(!openDrawer)}
+        >
+          <Box
+            role="presentation"
+            onClick={() => toggleDrawer(!openDrawer)}
+            onKeyDown={() => toggleDrawer(!openDrawer)}
+            component="div"
+            sx={{
+              width: "auto",
+            }}
+          >
+            <List>
+              <ListItem key={"home"} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <HomeOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={t("menuHome")} />
+                </ListItemButton>
+              </ListItem>
+            </List>
+            <List>
+              <ListItem key={"about"} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Face3OutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={t("menuAbout")} />
+                </ListItemButton>
+              </ListItem>
+            </List>
+            <List>
+              <ListItem key={"contact"} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <ContactPageOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={t("menuContact")} />
+                </ListItemButton>
+              </ListItem>
+            </List>
           </Box>
-        </Grid>
+        </Drawer>
         <Grid xs={2}>
           <Box display={"flex"} justifyContent={"right"}>
             {LanguageToggle}
